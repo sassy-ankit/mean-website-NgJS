52ec740ef9bf75f42f02f0793a688036eda495c6
Ankitkumar Chheda <ankitkumar.chheda@gmail.com>
2018-02-17 17:40:18 +0530

    Basic routing for home and api for user register

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var router = express.Router();
 var logger = require('morgan');
 var cors = require('cors');
 var path = require('path');
@@ -7,25 +8,38 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-// Definition of Routes
+// Routes
+var appRoutes = require('./app/routes/api')(router);
+
+// DB connection
+var dbURL = 'mongodb://localhost:27017/mean-website';
+mongoose.connect(dbURL, err => {
+	if (err) {
+		console.log('Error connecting DB: ', err);
+	} else {
+		console.log('## Connected to DB');
+	}
+});
 
 //middleware
-app.use(logger('dev'));
 app.use(cors());
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(logger('dev'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api', appRoutes);
 
-// Routes
+//Routes
+// GET "HOME"
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.sendFile(path.join(__dirname, './public/app/views/index.html'));
 });
 
 // Starting Server
 var port = process.env.PORT || 3000;
 app.listen(port, err => {
 	if (err) {
-		console.log('Error on instantiating server: ', err);
+		console.log('## Error on instantiating server: ', err);
 	}
-	console.log('Server listening on port ', port);
+	console.log('## Server listening on port ', port);
 });
